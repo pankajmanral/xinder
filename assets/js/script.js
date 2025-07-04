f9d5d364f5ea21dcb0287ac8d7a7f74db68cab1a
Pankaj Manral <pankaj_manral@ildc-inlp.idealake.com>
2025-07-04 17:36:00 +0530

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,42 +51,55 @@ document.addEventListener("DOMContentLoaded", () => {
         grabCursor: true,
         slidesPerView: 1,
         speed: 800,
-        autoplay: {
-            delay: 1500
-        },
-        breakpoints: {
-            0: {
-                autoplay: {
-                    enabled: true,
-                    disableOnInteraction: false
-                }
-            },
-            991: {
-                autoplay: {
-                    enabled: false
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-            }
+        // autoplay: {
+        //     delay: 1500
+        // },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         }
+        // breakpoints: {
+        //     0: {
+        //         autoplay: {
+        //             enabled: true,
+        //             disableOnInteraction: false
+        //         }
+        //     },
+        //     991: {
+        //         autoplay: {
+        //             enabled: false
+        //         },
+        //         navigation: {
+        //             nextEl: '.swiper-button-next',
+        //             prevEl: '.swiper-button-prev',
+        //         },
+        //     }
+        // }
 
     });
 
 
     // be incredible swiper
 
-    const swiper = new Swiper('.beincredible-swiper', {
+    const beincredibleSwiper = new Swiper('.beincredible-swiper', {
         loop: true,
-        slidesPerView: 3,
-        centeredSlides: true,
         grabCursor: true,
         speed: 800,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        breakpoints: {
+            0: {
+                centeredSlides: false,
+                slidesPerView: 1.2,
+                spaceBetween: 24
+            },
+            991: {
+                centeredSlides: true,
+                slidesPerView: 3,
+            }
+        }
     })
 
 });
